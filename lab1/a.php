568d03e962127c70cfda164194563ed041c3c2be
Bupdethuong <111048466+Bupdethuong@users.noreply.github.com>
2022-08-11 15:49:35 +0700

    Add files via upload

---
+++ lab1/a.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Chào các bạn</title>
+</head>
+
+<body>
+<?php
+echo "we ẩe nubme";
+?>
+</body>
+</html>
